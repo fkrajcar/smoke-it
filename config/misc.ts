f9c8adaf3d3c6d30da70ed600d223550eb000aaf
Filip Krajcar <25937081+fkrajcar@users.noreply.github.com>
2022-08-31 20:23:37 +0200

    add vuki playerId

--- config/misc.ts
+++ config/misc.ts
@@ -4,5 +4,6 @@ export const config = {
     zix: '92111634-df87-4a37-9955-aaff879eb335',
     marjan_bajs: '335912fe-3a1c-4d29-9e27-49e23e1eac7e',
     thraxxboi: '57b14f41-41f0-4a5f-afd5-812a63de5873',
+    vuki656: '1f261d02-853d-40e8-aed3-fe85deb006a2',
   },
 }
