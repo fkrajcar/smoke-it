3df1eaca93707554ebb5a871298a1288f8c3d0cd
Filip Krajcar <25937081+fkrajcar@users.noreply.github.com>
2022-09-15 19:45:34 +0200

    Update misc.ts
    add new player ids

--- config/misc.ts
+++ config/misc.ts
@@ -5,5 +5,7 @@ export const config = {
     marjan_bajs: '335912fe-3a1c-4d29-9e27-49e23e1eac7e',
     thraxxboi: '57b14f41-41f0-4a5f-afd5-812a63de5873',
     vuki656: '1f261d02-853d-40e8-aed3-fe85deb006a2',
+    edo215: 'f8b02166-0a21-4a9f-abd5-b2ce47350895',
+    sidr00n: '9087a0bb-3c68-43dc-a1d8-b49614ab9771',
   },
 }
