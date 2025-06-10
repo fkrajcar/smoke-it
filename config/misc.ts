9652b2424cd3e7c181aa1930bb6acd1b6c8bf304
fkrajcar <fkrajcar@proton.me>
2025-06-10 14:14:42 +0200

    fix: remove unused playerIds

--- config/misc.ts
+++ config/misc.ts
@@ -4,9 +4,6 @@ export const config = {
     zix: '92111634-df87-4a37-9955-aaff879eb335',
     marjan_bajs: '335912fe-3a1c-4d29-9e27-49e23e1eac7e',
     thraxxboi: '57b14f41-41f0-4a5f-afd5-812a63de5873',
-    vuki656: '1f261d02-853d-40e8-aed3-fe85deb006a2',
-    edo215: 'f8b02166-0a21-4a9f-abd5-b2ce47350895',
-    sidr00n: '9087a0bb-3c68-43dc-a1d8-b49614ab9771',
     rok: '3b8e55b2-e9c2-4553-8fd5-a29821df26f3',
   },
 }
